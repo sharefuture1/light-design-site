48d6f45974e2f1e6416a4f3873a428de600d7614
matrixAge <xiewendao123@foxmail.com>
2021-04-07 15:41:14 +0800

    [fix] 使用 useLayoutEffect

--- hooks/use_responsive.ts
+++ hooks/use_responsive.ts
@@ -1,13 +1,11 @@
-import { useState, useLayoutEffect, useEffect } from 'react'
+import { useState, useLayoutEffect } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
 const Index = () => {
 	const [ is_client, setIsClient ] = useState(false)
       const is_mobile = useMediaQuery({ maxWidth: 767 })
-      
-	const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
 
-	useIsomorphicLayoutEffect(() => {
+	useLayoutEffect(() => {
 		if (typeof window !== 'undefined') setIsClient(true)
 	}, [])
 
--- hooks/use_subscriptions.ts
+++ hooks/use_subscriptions.ts
@@ -1,11 +1,9 @@
-import { useState, useLayoutEffect, useEffect } from 'react'
+import { useState, useLayoutEffect } from 'react'
 
 const Index = <A, I>(app_state: A, init_state: I) => {
 	const [ state, setState ] = useState<A | I>(init_state)
-	
-	const useIsomorphicLayoutEffect =  typeof window !== 'undefined' ? useLayoutEffect : useEffect
 
-	useIsomorphicLayoutEffect(
+	useLayoutEffect(
 		() => {
 			setState(app_state)
 		},
