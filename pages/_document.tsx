4be37ba08fa1be1c79cc29ba93160fd9bb53cac0
matrixAge <xiewendao123@foxmail.com>
2021-04-07 10:09:51 +0800

    [add] manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,35 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const APP_NAME='Light Design'
+
 class Index extends Document {
 	static async getInitialProps (ctx) {
 		const initialProps = await Document.getInitialProps(ctx)
 
 		return { ...initialProps }
-	}
+      }
 
 	render () {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					<meta name='application-name' content={APP_NAME} />
+					<meta name='apple-mobile-web-app-capable' content='yes' />
+					<meta
+						name='apple-mobile-web-app-status-bar-style'
+						content='default'
+					/>
+					<meta name='apple-mobile-web-app-title' content={APP_NAME} />
+					<meta name='format-detection' content='telephone=no' />
+					<meta name='mobile-web-app-capable' content='yes' />
+					<meta name='theme-color' content='black' />
+					<link
+						rel='apple-touch-icon'
+						sizes='180x180'
+						href='/icons/apple-touch-icon.png'
+					/>
+					<link rel='manifest' href='/manifest.json' />
+				</Head>
 				<style id='holderStyle' type='text/css'>{`
                               *, *::before, *::after {
                                     transition: none !important;
