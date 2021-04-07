5b7dc32fe1f999c8f04708bf71156229d7117eaa
matrixAge <xiewendao123@foxmail.com>
2021-04-07 16:00:37 +0800

    [update] 更新sw规则

--- hooks/use_pwa.ts
+++ hooks/use_pwa.ts
@@ -3,23 +3,19 @@ import { message } from 'antd'
 
 const Index = () => {
 	useEffect(() => {
-		if (
-			typeof window !== 'undefined' &&
-			'serviceWorker' in navigator &&
-			window.workbox !== undefined
-		) {
-			const wb = window.workbox
+		if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+			let refreshing = false
 
-			wb.addEventListener('waiting', () => {
-				wb.addEventListener('controlling', () => {
-					message.warning('检测到文件更新，3s后自动刷新以更新页面')
+			navigator.serviceWorker.addEventListener('controllerchange', () => {
+				if (refreshing) return
 
-					setTimeout(() => {
-						window.location.reload()
-					}, 3000)
-				})
+				refreshing = true
 
-				wb.messageSkipWaiting()
+				message.warning('检测到文件更新，3s后自动刷新以更新页面')
+
+				setTimeout(() => {
+					window.location.reload()
+				}, 3000)
 			})
 		}
 	}, [])
