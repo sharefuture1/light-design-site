f6ffa168e13ad7f024559830a00d3937714bf5dc
matrixAge <xiewendao123@foxmail.com>
2021-04-08 17:20:29 +0800

    [test]

--- components/Index/Body/index.tsx
+++ components/Index/Body/index.tsx
@@ -7,7 +7,7 @@ const Index = () => {
 			className={`${styles._local} w_100vw h_100vh border_box flex justify_center align_center`}
 		>
 			<div className='content_wrap flex flex_column justify_center align_center'>
-				<span className='slogan cursor_point'>贴近自然的UI组件库123</span>
+				<span className='slogan cursor_point'>贴近自然的UI组件库</span>
 				<span className='description'>Light Design，自然之美</span>
                         <NavLink
                               className='btn_start flex justify_center align_center'
