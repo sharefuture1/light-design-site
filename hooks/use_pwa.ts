e4212f81fa86862ce90213ba30e987098d06b3c5
matrixAge <xiewendao123@foxmail.com>
2021-04-08 16:55:11 +0800

    [_]

--- hooks/use_pwa.ts
+++ hooks/use_pwa.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-// import { message } from 'antd'
+import { message } from 'antd'
 
 const Index = () => {
 	useEffect(() => {
@@ -18,21 +18,17 @@ const Index = () => {
 			// 	}, 1800)
 			// })
 
-			const promptNewVersionAvailable = () => {
-				if (
-					window.confirm(
-						'A newer version of this web app is available, reload to update?'
-					)
-				) {
-					wb.addEventListener('controlling', () => {
+			wb.addEventListener('waiting', () => {
+				wb.messageSkipWaiting()
+
+				wb.addEventListener('controlling', () => {
+					message.warning('检测到文件更新，2s后自动刷新以更新页面', 2)
+
+					setTimeout(async () => {
 						window.location.reload()
-					})
-
-					wb.messageSkipWaiting()
-				}
-			}
-
-			wb.addEventListener('waiting', promptNewVersionAvailable)
+					}, 1800)
+				})
+			})
 
 			wb.register()
 		}
