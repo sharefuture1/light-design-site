9b39708ca018ddbde71e8a47e1b75e2f50f9a010
matrixAge <xiewendao123@foxmail.com>
2021-04-14 10:55:46 +0800

    [add] 首页增加太极图 暗合自然日出日落之意

--- components/Index/Body/index.tsx
+++ components/Index/Body/index.tsx
@@ -6,13 +6,17 @@ const Index = () => {
 		<div
 			className={`${styles._local} w_100vw h_100vh border_box flex justify_center align_center`}
 		>
-			<div className='content_wrap flex flex_column justify_center align_center'>
-				<span className='slogan cursor_point'>贴近自然的UI组件库</span>
+			<div className='taichi_wrap fixed top_0 left_0 w_100vw h_100vh'>
+				<div className='taichi'>
+					<div className='white-circle' />
+					<div className='black-circle' />
+				</div>
+                  </div>
+                  <div className="taichi_mask fixed top_0 left_0 w_100vw h_100vh"></div>
+                  <div className='content_wrap flex flex_column justify_center align_center'>
+				<span className='slogan cursor_point transition_normal'>贴近自然的UI组件库</span>
 				<span className='description'>Light Design，自然之美</span>
-                        <NavLink
-                              className='btn_start flex justify_center align_center'
-					to='/com'
-				>
+				<NavLink className='btn_start flex justify_center align_center' to='/com'>
 					现在开始
 				</NavLink>
 			</div>
